1ebdf789bbb48fadc4f12ccc801714c5f768ed45
Oskar Madestam <oskar.madestam@ottoboni.se>
2017-08-16 16:10:40 +0200

    add multithreaded unittests

--- HaxkonstDB.Test/DeleteTest.cs
+++ HaxkonstDB.Test/DeleteTest.cs
@@ -6,54 +6,56 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace HaxkonstDB.Test
 {
-    [TestClass]
-    public class DeleteTest
-    {
+	[TestClass]
+	public class DeleteTest
+	{
 
-        private string dir = @"C:\Temp\db-test\deltetest\";
-        private Database databse;
+		private string dir = @"C:\Temp\db-test\deltetest\";
+		private Database databse;
 
-        [TestInitialize]
-        public void Init() {
-            databse = new Database(dir);
-        }
+		[TestInitialize]
+		public void Init() {
+			databse = new Database(dir);
+		}
 
-        [TestCleanup]
-        public void Cleanup() {
-            databse = null;
-            Directory.Delete(dir, true);
-        }
+		[TestCleanup]
+		public void Cleanup() {
+			databse = null;
+			Directory.Delete(dir, true);
+		}
 
-        [TestMethod]
-        public void DeleteObjectTest() {
-            var car = new Car() {
-                Driver = "Homer Simpson",
-                LicensePlate = "ADB789"
-            };
+		[TestMethod]
+		public void DeleteObjectTest() {
+			var car = new Car() {
+				Driver = "Homer Simpson",
+				LicensePlate = "ADB789"
+			};
 
-            databse.Create(car);
+			databse.Create(car);
 
-            var dbObjects = databse.Find<Car>(x => x.Driver == car.Driver);
-            Assert.AreEqual(1, dbObjects.Count());
+			var dbObjects = databse.Find<Car>(x => x.Driver == car.Driver);
+			Assert.AreEqual(1, dbObjects.Count());
 
-            databse.Delete(car);
+			databse.Delete(car);
 
-            dbObjects = databse.Find<Car>(x => x.Driver == car.Driver);
-            Assert.AreEqual(0, dbObjects.Count());
-        }
+			dbObjects = databse.Find<Car>(x => x.Driver == car.Driver);
+			Assert.AreEqual(0, dbObjects.Count());
+		}
 
-        [TestMethod]
-        [ExpectedException(typeof(DeleteUnexistingObjectException))]
-        public void DeletNonExistingObjectTest() {
-            var car = new Car() {
-                Driver = "Lisa Simpson",
-                LicensePlate = "AGB789"
-            };
-            databse.Delete(car);
-        }
+		[TestMethod]
+		[ExpectedException(typeof(DeleteUnexistingObjectException))]
+		public void DeletNonExistingObjectTest() {
+			var car = new Car() {
+				Driver = "Lisa Simpson",
+				LicensePlate = "AGB789"
+			};
+			databse.Delete(car);
+		}
 
     }
 }
--- HaxkonstDB.Test/UpdateTest.cs
+++ HaxkonstDB.Test/UpdateTest.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace HaxkonstDB.Test
 {
@@ -62,5 +64,65 @@ namespace HaxkonstDB.Test
             databse.Update(car);
         }
 
-    }
+		[TestMethod]
+		[ExpectedException(typeof(UpdateUnexistingObjectException))]
+		public void UpdateDeletedObject()
+		{
+			var boat = new Boat() {
+				Name = "Freja",
+				IsInUse = false
+			};
+			databse.Create(boat);
+
+			var t1 = new Task(() => {
+				var b = databse.Find<Boat>(x => !x.IsInUse).First();
+				Thread.Sleep(50);
+				databse.Delete(boat);
+			});
+			var t2 = new Task(() => {
+				var b = databse.Find<Boat>(x => !x.IsInUse).First();
+				t1.Wait();
+				boat.IsInUse = true;
+				databse.Update(boat);
+			});
+			t1.Start();
+			t2.Start();
+
+			t2.GetAwaiter().GetResult();
+		}
+
+		[TestMethod]
+		public void UpdateSameObjectsInDifernetTreads()
+		{
+			var boat = new Boat() {
+				Name = "Loveboat",
+				IsInUse = true,
+				YearlyMaintancenCost = 100.50M
+			};
+			databse.Create(boat);
+
+			var t1 = new Task(() => {
+				var b = databse.Find<Boat>(x => x.IsInUse).First();
+				b.YearlyMaintancenCost = 500M;
+				Thread.Sleep(50);
+				databse.Update(b);
+			});
+			var t2 = new Task(() => {
+				var b = databse.Find<Boat>(x => x.IsInUse).First();
+				t1.Wait();
+				Assert.AreEqual(100.50M, b.YearlyMaintancenCost);
+				b.YearlyMaintancenCost = 750M;
+				databse.Update(b);
+			});
+			t1.Start();
+			t2.Start();
+
+			t2.Wait();
+
+			var b2 = databse.Find<Boat>(x => x.IsInUse).First();
+			Assert.AreEqual(750M, b2.YearlyMaintancenCost);
+
+		}
+
+	}
 }
